8da5d8a7ee0f7a0a2d7e69e644720f61db8c33cb
Eugene Kravchenko <Zzzheka97@gmail.com>
2023-02-25 13:36:51 +0300

    Add 'custom' cargo feature

--- ceres-solver-sys/build.rs
+++ ceres-solver-sys/build.rs
@@ -5,14 +5,26 @@ fn main() {
 
     let mut cc_build = cxx_build::bridge("src/lib.rs");
     cc_build.file("src/lib.cpp");
-    cc_build.flag("-std=c++17");
-    #[cfg(feature = "source")]
+
+    #[cfg(all(feature = "source", feature = "custom"))]
+    compile_error!("Features 'source' and 'custom' are mutually exclusive.");
+
+    // flags
+    #[cfg(not(feature = "custom"))]
     {
-        cc_build.includes(std::env::split_paths(
-            &std::env::var("DEP_CERES_INCLUDE").unwrap(),
-        ));
+        cc_build.flag("-std=c++17");
     }
-    #[cfg(not(feature = "source"))]
+    #[cfg(feature = "custom")]
+    {
+        if let Ok(flags) = std::env::var("CERES_RS_FLAGS") {
+            for flag in flags.split_whitespace() {
+                cc_build.flag(flag);
+            }
+        }
+    }
+
+    // includes
+    #[cfg(not(any(feature = "source", feature = "custom")))]
     {
         // Helps on Ubuntu
         #[cfg(target_os = "linux")]
@@ -30,13 +42,34 @@ fn main() {
             cc_build.include("/opt/homebrew/include/eigen3");
         }
     }
-    cc_build.compile("ceres-solver-sys");
-
     #[cfg(feature = "source")]
     {
-        println!("cargo:rustc-link-lib=static=ceres");
+        cc_build.includes(std::env::split_paths(
+            &std::env::var("DEP_CERES_INCLUDE").unwrap(),
+        ));
     }
-    #[cfg(not(feature = "source"))]
+    #[cfg(feature = "custom")]
+    {
+        if let Ok(include_dirs) = std::env::var("CERES_RS_INCLUDE_DIRS") {
+            cc_build.includes(include_dirs.split_whitespace());
+        }
+    }
+
+    // defines
+    #[cfg(feature = "custom")]
+    {
+        if let Ok(defines) = std::env::var("CERES_RS_DEFINES") {
+            for define in defines.split_whitespace() {
+                if let Some((key, value)) = define.split_once('=') {
+                    cc_build.define(key, value);
+                }
+            }
+        }
+    }
+
+    cc_build.compile("ceres-solver-sys");
+
+    #[cfg(not(any(feature = "source", feature = "custom")))]
     {
         if let Err(pkg_config_error) = pkg_config::Config::new()
             // the earliest version tested, it may work with elder versions
@@ -46,5 +79,16 @@ fn main() {
             dbg!(pkg_config_error);
             println!("cargo:rustc-link-lib=ceres");
         }
+    }
+    #[cfg(feature = "source")]
+    {
+        println!("cargo:rustc-link-lib=static=ceres");
+    }
+    #[cfg(feature = "custom")]
+    {
+        if let Ok(lib_dir) = std::env::var("CERES_RS_LIB_DIR") {
+            println!("cargo:rustc-link-search=native={}", lib_dir);
+        }
+        println!("cargo:rustc-link-lib=static=ceres");
     }
 }
