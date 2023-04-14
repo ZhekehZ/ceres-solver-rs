948b58816eeeb5625ed3a9d0977989be7101a82d
Eugene Kravchenko <zzzheka97@gmail.com>
2023-04-14 10:21:33 +0300

    Make libs configurable from env when feature=custom

--- ceres-solver-sys/build.rs
+++ ceres-solver-sys/build.rs
@@ -9,11 +9,6 @@ fn main() {
     #[cfg(all(feature = "source", feature = "custom"))]
     compile_error!("Features 'source' and 'custom' are mutually exclusive.");
     
-    // flags
-    #[cfg(not(feature = "custom"))]
-    {
-        cc_build.flag("-std=c++17");
-    }
     #[cfg(feature = "custom")]
     {
         if let Ok(flags) = std::env::var("CERES_RS_FLAGS") {
@@ -21,30 +16,39 @@ fn main() {
                 cc_build.flag(flag);
             }
         }
-    }
-
-    // includes
-    #[cfg(feature = "source")]
-    {
-        cc_build.includes(std::env::split_paths(
-            &std::env::var("DEP_CERES_INCLUDE").unwrap(),
-        ));
-        println!("cargo:rustc-link-lib=static=glog");
-        println!("cargo:rustc-link-lib=static=ceres");
-    }
-    #[cfg(feature = "custom")]
-    {
         if let Ok(include_dirs) = std::env::var("CERES_RS_INCLUDE_DIRS") {
             cc_build.includes(include_dirs.split(','));
         }
         if let Ok(lib_dir) = std::env::var("CERES_RS_LIB_DIR") {
             println!("cargo:rustc-link-search=native={}", lib_dir);
         }
-        println!("cargo:rustc-link-lib=static=ceres");
+        if let Ok(libs) = std::env::var("CERES_RS_LIBS") {
+            for lib in libs.split(',') {
+                println!("cargo:rustc-link-lib=static={}", lib);
+            }
+        }
+        if let Ok(defines) = std::env::var("CERES_RS_DEFINES") {
+            for define in defines.split(',') {
+                if let Some((key, value)) = define.split_once('=') {
+                    cc_build.define(key, value);
+                }
+            }
+        }
+    }
+    #[cfg(feature = "source")]
+    {
+        cc_build.flag("-std=c++17");
+
+        cc_build.includes(std::env::split_paths(
+            &std::env::var("DEP_CERES_INCLUDE").unwrap(),
+        ));
         println!("cargo:rustc-link-lib=static=glog");
+        println!("cargo:rustc-link-lib=static=ceres");
     }
     #[cfg(not(any(feature = "source", feature = "custom")))]
     {
+        cc_build.flag("-std=c++17");
+
         if let Ok(library) = pkg_config::Config::new()
             .range_version("3.3.4".."4.0.0")
             .probe("eigen3")
@@ -61,18 +65,6 @@ fn main() {
                 cc_build.include(path);
             }),
             Err(_) => println!("cargo:rustc-link-lib=ceres"),
-        }
-    }
-
-    // defines
-    #[cfg(feature = "custom")]
-    {
-        if let Ok(defines) = std::env::var("CERES_RS_DEFINES") {
-            for define in defines.split(',') {
-                if let Some((key, value)) = define.split_once('=') {
-                    cc_build.define(key, value);
-                }
-            }
         }
     }
 
